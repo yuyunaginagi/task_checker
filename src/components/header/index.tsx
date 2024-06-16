fe4567bc33d2334aaca7ba278835e3de1207bfad
yuyunaginagi <centrepg8@gmail.com>
2024-06-16 21:18:36 +0900

    ヘッダーのマークアップ実装準備

---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+【ここを編集】
+【ここを編集】
+
+export const Header = () => {
+  return (
+    <div className="【ここを編集】">
+      <DoneAll className="【ここを編集】" fontSize="large"></DoneAll>
+      <span className="【ここを編集】">Task Checker</span>
+    </div>
+  );
+};
