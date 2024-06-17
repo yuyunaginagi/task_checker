f78c43e840d9d33f31ef928bd165258b4818907d
yuyunaginagi <centrepg8@gmail.com>
2024-06-17 15:35:15 +0900

    ヘッダーのマークアップ

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-【ここを編集】
-【ここを編集】
+import DoneAll from "@material-ui/icons/DoneAll";
+import "./style.css";
 
 export const Header = () => {
   return (
-    <div className="【ここを編集】">
-      <DoneAll className="【ここを編集】" fontSize="large"></DoneAll>
-      <span className="【ここを編集】">Task Checker</span>
+    <div className="header">
+      <DoneAll className="header_icon" fontSize="large"></DoneAll>
+      <span className="header_title">Task Checker</span>
     </div>
   );
 };
