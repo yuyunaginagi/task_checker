705f63046808ff7a902af0012184fb82fe852459
yuyunaginagi <centrepg8@gmail.com>
2024-06-23 13:49:15 +0900

    taskRequest関数を定義

--- src/requests/taskRequest.ts
+++ src/requests/taskRequest.ts
@@ -1,6 +1,12 @@
 import axiosBase from "axios";
 
+type action = "fetchTasks" | "createTasks";
+
 const api = axiosBase.create({
   baseURL: "http://localhost:3001/tasks",
   responseType: "json",
 });
+
+export const taskRequest: (action: action) => any = (action: action) => {
+
+};
