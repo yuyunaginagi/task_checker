4c075f9279c0f895e6d2747307ef8d2b3e41e75a
yuyunaginagi <centrepg8@gmail.com>
2024-06-28 22:14:39 +0900

    ジャンルデータを追加

--- src/requests/genreRequest.ts
+++ src/requests/genreRequest.ts
@@ -1,11 +1,34 @@
 import axiosBase from "axios";
+import { GenreType } from "../interfaces/GenreType";
 
 type action = "fetchGenres" | "createGenres";
+
+type parameter = { data: GenreType };
 
 const api = axiosBase.create({
   baseURL: "http://localhost:3001/genres",
   responseType: "json",
 });
+
+export const genreRequest: (action: action, parameter?: parameter) => any = async (action: action, parameter?: parameter) => {
+  if(parameter) {
+    switch (action) {
+      case "createGenres":
+        const createGenres = await api.post("/", parameter.data);
+        return createGenres.data;
+      default:
+        return null;
+    }
+  } else {
+    switch (action) {
+      case "fetchGenres":
+        const fetchGenres await api.get("/");
+        return fetchGenres.data;
+      default:
+        return null;
+    }
+  }
+};
 
 export const genreRequest: (action: action) => any = async (action: action) => {
   switch (action) {
